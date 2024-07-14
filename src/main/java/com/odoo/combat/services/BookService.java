8e0cc3bdce079255019d0c9eb63dc5c7dd258084
yash655 <yvmehta2003@gmail.com>
2024-07-14 12:25:08 +0530

    y3

--- src/main/java/com/odoo/combat/services/BookService.java
+++ src/main/java/com/odoo/combat/services/BookService.java
@@ -6,8 +6,12 @@ import java.util.List;
 import com.odoo.combat.api.v1.dtos.Root;
 
 public interface BookService {
-	public Root getBookInfoByIsbn(String isbn);
 	
+	public List<Root> getBookInfoBySearch(String search);
+
+	public Root getBookInfoByIsbn(String isbn);
+	public Root getBookInfoByBookName(String bookname);
+
 	public List<Root> getBookInfoByGenres(String Genres);
 	
 	public List<Root> getBookInfoByAuthor(String author);
