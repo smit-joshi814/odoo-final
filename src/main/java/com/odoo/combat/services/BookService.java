58cdd1bc28264ff679f401a22e6bf365169acc92
Smit Joshi <smitjoshi814@gmail.com>
2024-07-14 09:58:10 +0530

    BookService

---
+++ src/main/java/com/odoo/combat/services/BookService.java
@@ -0,0 +1,5 @@
+package com.odoo.combat.services;
+
+public interface BookService {
+
+}
---
+++ src/main/java/com/odoo/combat/services/impl/BookServiceImpl.java
@@ -0,0 +1,7 @@
+package com.odoo.combat.services.impl;
+
+import com.odoo.combat.services.BookService;
+
+public class BookServiceImpl implements BookService {
+
+}
